241f9f5610494f9c6140b215bbdf7a8238cb0898
HoangMath1995 <156791618+HoangMath1995@users.noreply.github.com>
2025-08-21 20:20:51 +0700

    Update PlayerInfo.java

--- footballPlayerData/src/main/java/com/example/entity/PlayerInfo.java
+++ footballPlayerData/src/main/java/com/example/entity/PlayerInfo.java
@@ -1,6 +1,46 @@
 package com.example.entity;
 
 public class PlayerInfo {
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public String getIndexName() {
+        return indexName;
+    }
+
+    public void setIndexName(String indexName) {
+        this.indexName = indexName;
+    }
+
+    public int getPlayerAge() {
+        return playerAge;
+    }
+
+    public void setPlayerAge(int playerAge) {
+        this.playerAge = playerAge;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getPlayerIndexId() {
+        return playerIndexId;
+    }
+
+    public void setPlayerIndexId(int playerIndexId) {
+        this.playerIndexId = playerIndexId;
+    }
+
     private int playerIndexId;
     private String playerName;
     private int playerAge;
@@ -9,3 +49,5 @@ public class PlayerInfo {
 
     // Constructor, Getters and Setters
 }
+
+
