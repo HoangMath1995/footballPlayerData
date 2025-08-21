1d05fe95deb92db926d388776199bc0f151d0139
HoangMath1995 <156791618+HoangMath1995@users.noreply.github.com>
2025-08-21 20:23:00 +0700

    Update Player.java

--- footballPlayerData/src/main/java/com/example/entity/Player.java
+++ footballPlayerData/src/main/java/com/example/entity/Player.java
@@ -5,5 +5,28 @@ public class Player {
     private String name;
     private int age;
 
-    // Getters and Setters
+        public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
+
